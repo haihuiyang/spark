4f63f570b6130d5303b1b143efd6a2afd0cd9d2b
yanghaihui <yanghaihui@betalpha.com>
2016-10-30 23:47:07 +0800

    optimize code.

--- src/main/scala/com/yhh/spark/App.scala
+++ src/main/scala/com/yhh/spark/App.scala
@@ -17,37 +17,62 @@ object App {
 
     println("cassandra example: ")
 
-    SparkContext
+    import org.apache.spark.SparkConf
+
+    // you don't have to declare a sparkContext, it's not required.
+    val conf = new SparkConf()
+      .setMaster("local[2]")
+      .set("spark.cassandra.connection.host", "localhost")
+      .set("spark.some.config.option", "some-value")
+      .setAppName("example app.")
+
+    val sc = new SparkContext(conf)
+
+    //    SparkContext
     val spark = SparkSession
       .builder()
-      .appName("Spark cassandra Example")
-      .config("spark.some.config.option", "some-value")
-      .config("spark.cassandra.connection.host", "localhost")
-      .master("local[4]")
-      .appName("cassandraTest")
+      //      .appName("Spark cassandra Example")
+      //      .config("spark.some.config.option", "some-value")
+      //      .config("spark.cassandra.connection.host", "localhost")
+      //      .config("spark.cassandra.connection.host", ""127.0.0.1,127.0.0.2"")
+      //      .master("local[4]")
+      //      .master("spark://master:7077")
+      //      .appName("cassandraTest")
       .getOrCreate()
 
     val load: DataFrame = spark.read.format("org.apache.spark.sql.cassandra")
       .options(Map("table" -> "idx_weight", "keyspace" -> "gta")).load()
 
-    log.warn("load idx_weight from gta keyspace.")
+    log.info("load idx_weight from gta keyspace.")
 
     load.createOrReplaceTempView("idx_weight")
 
     import spark.sql
 
     val sqlDF1 = sql("select symbol,sampleSecurityCode,tradingDate, WEIGHT from idx_weight where sampleSecurityCode >= \'600004\' and" +
-      " sampleSecurityCode <= \'600008\'")
+      " sampleSecurityCode <= \'600008\' and weight > 0.5")
 
     sqlDF1.show(10)
+
+    /*
+    window functions.
+     */
+    import org.apache.spark.sql.expressions.Window
+    import org.apache.spark.sql.functions._
+
+    val wSpec = Window.partitionBy("symbol").orderBy("tradingDate").rowsBetween(Long.MinValue, 1)
+
+    sqlDF1.withColumn("avg", sum(sqlDF1("WEIGHT")).over(wSpec)).show()
+
+
     println("get data from cassandra db.")
 
     println("\n\n\n")
 
     println("sql example: ")
 
-    SQLContext
-    val jdbcDF = spark.read.format("jdbc")
+    //    SQLContext
+    val loadSQL: DataFrame = spark.read.format("jdbc")
       .option("driver", "com.mysql.jdbc.Driver")
       .option("url", "jdbc:mysql://localhost:3306/test?useSSL=false&serverTimezone=UTC")
       .option("dbTable", "test")
@@ -55,11 +80,14 @@ object App {
       .option("password", "root")
       .load()
 
-    jdbcDF.createOrReplaceTempView("idx_weight")
+    loadSQL.createOrReplaceTempView("idx_weight")
+    loadSQL.printSchema()
 
     val sqlDF2 = sql("select * from idx_weight")
 
     sqlDF2.show(20)
+
+    sc.stop() //stop sparkContext.
   }
 
   println("Hello World!")
