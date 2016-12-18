f7c90d0183cbca2ae335905c4fc1445abd7e7945
yanghaihui <yanghaihui@betalpha.com>
2016-12-18 15:22:34 +0800

    change code layout.

--- src/main/scala/com/yhh/examples/ReadAndWriteFromDifferentHost.scala
+++ src/main/scala/com/yhh/examples/ReadAndWriteFromDifferentHost.scala
@@ -12,7 +12,10 @@ object ReadAndWriteFromDifferentHost extends DefaultSparkConf {
   //If not, you should make some conversions.
   def Cassandra1ToCassandra2(): Unit = {
     spark.read.format("org.apache.spark.sql.cassandra")
-      .options(Map("spark.cassandra.connection.host" -> "host1", "keyspace" -> "ks1", "table" -> "tb1"))
+      .options(Map(
+        "spark.cassandra.connection.host" -> "host1",
+        "keyspace" -> "ks1",
+        "table" -> "tb1"))
       .load()
       .createTempView("tb1")
 
@@ -20,7 +23,9 @@ object ReadAndWriteFromDifferentHost extends DefaultSparkConf {
     df.show(20)
 
     df.write.format("org.apache.spark.sql.cassandra")
-      .options(Map("spark.cassandra.connection.host" -> "host2", "keyspace" -> "tb2", "table" -> "tb2"))
+      .options(Map(
+        "spark.cassandra.connection.host" -> "host2",
+        "keyspace" -> "tb2", "table" -> "tb2"))
       .mode(SaveMode.Append).save()
   }
 
@@ -28,13 +33,21 @@ object ReadAndWriteFromDifferentHost extends DefaultSparkConf {
   //If not, you should make some conversions.
   def MysqlToCassandra(): Unit = {
     val mysqlDF = spark.read.format("jdbc")
-      .options(Map("driver" -> "com.mysql.jdbc.Driver", "url" -> "jdbc:mysql://host1:3306/database?useSSL=false&serverTimezone=UTC", "dbTable" -> "table", "user" -> "root", "password" -> "root"))
+      .options(Map(
+        "driver" -> "com.mysql.jdbc.Driver",
+        "url" -> "jdbc:mysql://host1:3306/database?useSSL=false&serverTimezone=UTC",
+        "dbTable" -> "table",
+        "user" -> "root",
+        "password" -> "root"))
       .load()
 
     mysqlDF.show(20)
 
     mysqlDF.write.format("org.apache.spark.sql.cassandra")
-      .options(Map("spark.cassandra.connection.host" -> "host2", "keyspace" -> "ks1", "table" -> "tb1"))
+      .options(Map(
+        "spark.cassandra.connection.host" -> "host2",
+        "keyspace" -> "ks1",
+        "table" -> "tb1"))
       .mode(SaveMode.Append)
       .save()
   }
