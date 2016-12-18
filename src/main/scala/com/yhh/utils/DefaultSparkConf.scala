4f4cb69a7116f34548ab5dea4fcacfa491021587
yanghaihui <yanghaihui@betalpha.com>
2016-12-18 15:39:05 +0800

    add SparkContext and SparkSession explains.

--- src/main/scala/com/yhh/utils/DefaultSparkConf.scala
+++ src/main/scala/com/yhh/utils/DefaultSparkConf.scala
@@ -13,8 +13,13 @@ trait DefaultSparkConf {
     .setMaster("local")
     .set("spark.cassandra.connection.host", "localhost")
 
+  /*
+  SparkContext是开发Spark应用的入口，它负责和整个集群的交互，包括创建RDD，accumulators and broadcast variables。
+  Spark默认的构造函数接受org.apache.spark.SparkConf， 通过这个参数我们可以自定义本次提交的参数，这个参数会覆盖系统的默认配置。
+   */
   val sc = new SparkContext(conf)
 
+  //gets a existing sparkSession or, if there is no existing one,creates a new one.
   val spark = SparkSession
     .builder()
     .getOrCreate()
